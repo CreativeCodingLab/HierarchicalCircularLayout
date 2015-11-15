e44510b7e600f3b468c2d61fec5a7f0ff6f5779a
Paul Murray <plmrry@gmail.com>
2015-11-14 21:14:12 -0600

    clean up code; simplify radial layout

--- paul/hcl.js
+++ paul/hcl.js
@@ -7,14 +7,7 @@ var treeData = stream.fromCallback(d3.json)("flare.json")
   
 treeData
   .subscribe(function(root) { 
-    var nodes = tree.nodes(root),
-        links = tree.links(nodes);
-  
-    var link = svg.selectAll(".link")
-        .data(links)
-      .enter().append("path")
-        .attr("class", "link")
-        .attr("d", diagonal);
+    var nodes = tree.nodes(root);
   
     var node = svg.selectAll(".node")
         .data(nodes)
@@ -25,21 +18,7 @@ treeData
     node.append("circle")
         .attr("r", 4.5);
   
-    node.append("text")
-        .attr("dy", ".31em")
-        .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-        .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-        .text(function(d) { return d.name; });
   });
-
-// stream.create(function(observer) {
-//   d3.json(file, function(d) { observer.onNext(d); observer.onCompleted() });
-// }).subscribe(function(d) { console.log(d); });
-
-// new Promise(function(resolve) { d3.json(file, resolve); })
-//   .then(function(classes) {
-//     var tree = getTree(classes);
-//   });
 
 var diameter = 500;
 
@@ -54,34 +33,6 @@ var svg = d3.select("body").append("svg")
     .attr("height", "500")
   .append("g")
     .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
-
-// d3.json("flare.json", function(error, root) {
-//   if (error) throw error;
-
-//   var nodes = tree.nodes(root),
-//       links = tree.links(nodes);
-
-//   var link = svg.selectAll(".link")
-//       .data(links)
-//     .enter().append("path")
-//       .attr("class", "link")
-//       .attr("d", diagonal);
-
-//   var node = svg.selectAll(".node")
-//       .data(nodes)
-//     .enter().append("g")
-//       .attr("class", "node")
-//       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
-
-//   node.append("circle")
-//       .attr("r", 4.5);
-
-//   node.append("text")
-//       .attr("dy", ".31em")
-//       .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-//       .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-//       .text(function(d) { return d.name; });
-// });
 
 function setupTree() {
   var disFactor = 2;
@@ -122,71 +73,8 @@ function setupTree() {
       });
     }
     scaleRate = height/(height-minY);
-  //  console.log(" minY = "+minY +"  "+scaleRate);
-   // console.log("maxDepth = "+maxDepth);
+
     return d;
   });
-  /// Restart the force layout.
-  //  force.nodes(newNodes);
-  //  force.links(linkTree);
-  //  force.start();
-
     
 }  
-
-
-
-// // Lazily construct the package hierarchy from class names.
-// function packageHierarchy(classes) {
-//   var map = {};
-//   function find(name, data) {
-//     var node = map[name], i;
-//     if (!node) {
-//       node = map[name] = data || {name: name, children: []};
-//       if (name.length) {
-//         node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
-//         node.parent.children.push(node);
-//         node.key = name.substring(i + 1);
-//       }
-//     }
-//     return node;
-//   }
-//   classes.forEach(function(d) {
-//     find(d.name, d);
-//   });
-//   return map[""];
-// }
-
-// // Return a list of imports for the given array of nodes.
-// function packageImports(nodes) {
-//   var map = {},
-//       imports = [];
-//   // Compute a map from name to node.
-//   nodes.forEach(function(d) {
-//     map[d.name] = d;
-//   });
-//   // For each import, construct a link from the source to target node.
-//   nodes.forEach(function(d) {
-//     if (d.imports) 
-//       d.imports.forEach(function(i) {
-//         imports.push({source: map[d.name], target: map[i]});
-//     });
-//   });
-//   return imports;
-// }
-
-
-// function getTree(data) {
-//   return packageHierarchy(data);
-// }
-
-// function getLinksFromNodes(nodes) {
-//   return packageImports(nodes);
-// }
-  
-// var alreadyTree = stream.fromCallback(d3.json)("flare.json")
-//   .map(function(a) { return a[1]; })
-  
-// var flat = stream.fromCallback(d3.json)(file)
-//   .map(function(a) { return a[1]; })
-  
