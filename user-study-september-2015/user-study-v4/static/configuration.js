1866ece379f813a7048641108c4e5ee21206fddd
Paul Murray <plmrry@gmail.com>
2015-11-30 06:52:45 -0600

    clean up compiled coffeescript;

--- user-study-september-2015/user-study-v4/static/configuration.js
+++ user-study-september-2015/user-study-v4/static/configuration.js
@@ -1,17 +1,22 @@
 /* global _, d3 */
 
-var addContinue, addHtml, addVis, continue_button, dataPath, layouts, pages, part_1, part_1_nested, queryDatasets, randomList, seed, subtreeQuestion;
+var addContinue, addHtml, addVis, continue_button, dataPath, layouts, pages, 
+part_1, part_1_nested, queryDatasets, randomList, seed, subtreeQuestion;
 continue_button = ["Continue"];
 addVis = function(main, layout, queryData, randomSeed, height, degree, treeOnly, hasSubtree, showSubtree) {
   var v;
   v = main.selectAll('.row.vis').data(Array(1));
-  return v.enter().append('div').classed('row vis', true).append('div').classed('col-xs-12', true).append('div').classed('frame', true).style({
-    width: "100%",
-    height: "70vh",
-    border: "1px solid #ccc"
-  }).call(function(frame) {
-    return window[layout](queryData, randomSeed, height, degree, frame, treeOnly, hasSubtree, showSubtree);
-  });
+  return v.enter()
+    .append('div').classed('row vis', true)
+    .append('div').classed('col-xs-12', true)
+    .append('div').classed('frame', true)
+    .style({
+      width: "100%",
+      height: "70vh",
+      border: "1px solid #ccc"
+    }).call(function(frame) {
+      return window[layout](queryData, randomSeed, height, degree, frame, treeOnly, hasSubtree, showSubtree);
+    });
 };
 subtreeQuestion = function(pageOptions) {
   return function(parentOptions) {
@@ -22,28 +27,37 @@ subtreeQuestion = function(pageOptions) {
     queryData = pageOptions.queryData, layout = pageOptions.layout, randomSeed = pageOptions.randomSeed;
     buttons = [0, 1, 2, 3, 4];
     startTime = new Date().getTime();
+    
     console.info("Data: %s", queryData);
     console.info("Layout: %s", layout);
     console.info("Seed: %s", randomSeed);
+    
     html = "How many times does the subtree on the right appear in the tree on the left?";
     addHtml(main, html);
     addVis(main, layout, queryData, randomSeed, 6, 6, true, true, true);
-    choices = main.insert('div', '.vis').classed('row', true).append('div').classed('col-xs-12', true).append('div').classed('btn-group', true).attr({
-      'data-toggle': 'buttons'
-    }).selectAll('label').data(buttons);
-    choices.enter().append('label').classed('btn btn-lg btn-secondary', true).call(function(label) {
-      label.append('input').attr({
-        type: 'radio',
-        name: 'options',
-        autocomplete: 'off',
-        id: function(d, i) {
-          return "option" + i;
-        }
+    
+    choices = main.insert('div', '.vis').classed('row', true)
+      .append('div').classed('col-xs-12', true)
+      .append('div').classed('btn-group', true)
+      .attr({
+        'data-toggle': 'buttons'
+      }).selectAll('label').data(buttons);
+      
+    choices.enter()
+      .append('label').classed('btn btn-lg btn-secondary', true)
+      .call(function(label) {
+        label.append('input').attr({
+          type: 'radio',
+          name: 'options',
+          autocomplete: 'off',
+          id: function(d, i) {
+            return "option" + i;
+          }
+        });
+        label.append(function(d) {
+          return document.createTextNode(d);
+        });
       });
-      return label.append(function(d) {
-        return document.createTextNode(d);
-      });
-    });
     confirm = main.insert('div', '.vis').classed('row', true).append('div').classed('col-xs-12', true).append('button').classed('btn btn-lg btn-secondary', true).style({
       'margin-bottom': '0.5rem'
     }).attr({
